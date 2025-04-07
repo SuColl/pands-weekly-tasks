f9dd9d6bac35510ccb436ca5a859c6c0a5413b25
Susan <susanbot@gmail.com>
2025-04-07 22:48:23 +0100

    week03 v02

--- accounts.py
+++ accounts.py
@@ -1,12 +1,19 @@
-# accounts.py
-# This program reads in a 10 character account number and outputs the account number with only the last 4 digits showing (and the first 6 digits replaced with the character X.
+# accounts.py v02
+# This program reads in an account number of unknown length, and outputs the account number with only the last 4 digits showing (and the leading digits replaced with the character X.)
 # author: Susan Collins
+# v01 assumed that a 10-digit number would be entered. 
 
-# get input string. As the account number is described as "10-character", I assume it is alphanumeric, and cast it as a string.
-account_number = str(input("Please enter a 10 digit account number: "))
+# get input string. As the account number was originally described as "10-character", I assume it may contain both digits and letters, and cast it as a string.
+account_number = str(input("Please enter an account number: "))
 
-# Assuming the input is valid and has 10 characters, extract the last 4 characters of the string
+# get length of the input string
+account_number_length = len(account_number)
+
+# Assuming the input is valid, extract the last 4 characters of the string. If the original account number has four characters or fewer, it will output without obfuscation.
 account_number_tail = account_number[-4:]
 
-# print the obfuscated account number
-print(f"XXXXXX{account_number_tail}")
+# Create a string to represent the obfuscated part of the account number (i.e. 4 characters shorter than the original account number). If the original account number has 4 characters or fewer, the obfuscated part will be an empty string.
+account_number_obfuscation = "X" * (account_number_length - 4)
+
+# print the concatenated obfuscated account number
+print(f"{account_number_obfuscation}{account_number_tail}")

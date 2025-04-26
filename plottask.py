d5dfe07123eed339cb916e4a0bdcc13d23baf58b
Susan <susanbot@gmail.com>
2025-04-26 17:32:10 +0100

    feat:(plottask) Add second plot of cubic function

--- plottask.py
+++ plottask.py
@@ -11,12 +11,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# Parameters for set of 1000 normally distributed data points
+# Parameters to generate set of 1000 normally distributed data points
 distrib_mean = 5
 distrib_stddev = 2
 n_distrib_values = 1000
 
-# The NumPy documentation states that the RandomState function used in lectures has be superseded by the Generator method.
+# The NumPy documentation states that the RandomState function used in lectures has been superseded by the Generator method.
 # This method taken from https://numpy.org/doc/stable/reference/random/generated/numpy.random.Generator.choice.html
 # Seed the random number generator, for debugging purposes
 rng = np.random.default_rng(seed=1)
@@ -24,5 +24,19 @@ rng = np.random.default_rng(seed=1)
 distrib_values = rng.normal(distrib_mean, distrib_stddev, n_distrib_values)
 
 
-# Print to test
-print(distrib_values)
+# Cubic function parameters:
+range_min = 0
+range_max = 11  # set to 11 as I want to include the point x=10
+index = 3
+# Generate the data
+x_values = np.arange(range_min,range_max)
+y_values = x_values ** index
+
+
+# Plot the histogram of the normally distributed data points
+plt.hist(distrib_values)
+# Plot the function y=x^2, on the same plot
+plt.plot(x_values,y_values)
+
+# Display to screen
+plt.show()

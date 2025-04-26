c3cddadddedc2fe67de445cdae8ee6c0e6035786
Susan <susanbot@gmail.com>
2025-04-26 22:12:23 +0100

    feat:(es) Add optional parameter to specify which character to count

--- es.py
+++ es.py
@@ -4,22 +4,23 @@
 # Author: Susan Collins
 
 # v01: basic structure only, no error-checking yet
-# v02: error checking for inputs
+# v02: error checking 
+# v03: option to specify letter to count
+
 
 # Library to allow the passing of command-line arguments. If the module is not installed, program ends. 
 try:
     import sys
 except ModuleNotFoundError:
-    print("This program required the module 'sys'. Please install this module and try again.")
+    print("This program required the module 'sys'. Please install this module and try again. \nGoodbye.")
     exit()
 
-# Specify letter to be counted. The count will combine uppercase and lowercase.
-letter_to_count = 'e'
 
 # Function to print help message, called if there are no command-line arguments specified with this program. This function uses argv[0] to dynamically print the program name. 
 def print_help():
-    print(f"This is a program to count the instanced of the letter 'e' in a text file.")
-    print(f"This program should be called as: $ python {sys.argv[0]} <FILE.TXT>")
+    print(f"This is a program to count the instances of the letter 'e' in a text file. \nYou may choose a different letter to count by specifying it on the command line.")
+    print(f"This program should be called as: $ python {sys.argv[0]} <FILE.TXT> <optional letter>")
+
 
 # Function to count the instances of a letter in a given string. Returns the count as an integer.
 def count_letters(string, letter):
@@ -40,13 +41,35 @@ if len(sys.argv) < 2:
     print_help()
     exit()
 
+
 # get text file name as command-line argument 
 FILENAME = sys.argv[1]
 print(f"filename is {FILENAME}")
 
+
+
+# Get letter to be counted as optional command-line argument. If no letter is specified, the letter 'e' is counted. The count will combine uppercase and lowercase instances.
+try: 
+    # get letter as next command-line argument, stripped of any whitespace
+    letter_to_count = sys.argv[2].strip()       
+
+except IndexError:
+    # if there is no second command-line argument, the sys.argv[2].strip() line above will fail with IndexError
+    letter_to_count = 'e'                      
+
+except NameError: 
+    # if for any other reason, letter_to_count fails to have a value assigned
+    letter_to_count = 'e'
+
+else:
+    # if the second command-line argument has more than one character, program ends. Single numbers are accepted.
+    if len(letter_to_count) > 1:                
+        print(f"This program can only count single letters, and you asked it to count {letter_to_count}. \nGoodbye.")
+        exit()
+
+
 # declare variable to count the instances of 'e's
 count = 0
-
 # open the specified text file read-only 
 try:
     with open (FILENAME, 'rt') as text_file:
@@ -55,7 +78,6 @@ try:
             count += count_letters(line, letter_to_count)
             
         print(f"Finished - there are {count} instance(s) of the letter {letter_to_count} in the file {FILENAME} ")
-
 
 # Exception if the file does not exist
 except FileNotFoundError:

29cc976728557ab116b95307398f04cc451706a8
Susan <susanbot@gmail.com>
2025-04-27 20:07:36 +0100

    feat:(plottask) Added histogram bins centred on integers

--- plottask.py
+++ plottask.py
@@ -1,6 +1,6 @@
 # this program displays: 
-#  - a histogram of a normal distribution of a 1000 values with a mean of 5 
-# and standard deviation of 2, 
+#  - a histogram of a normal distribution of a 1000 values 
+# with a mean of 5 and standard deviation of 2, 
 #  - and a plot of the function  h(x)=x3 in the range 0 to 10, 
 #  on the one set of axes.
 
@@ -27,18 +27,38 @@ distrib_values = rng.normal(distrib_mean, distrib_stddev, n_distrib_values)
 
 
 # Cubic function parameters:
-range_min = 0
-range_max = 11  # set to 11 as I want to include the point x=10
+cubic_range_min = 0
+cubic_range_max = 11  # set to 11 as I want to include the point x=10
 index = 3
 # Generate the data
-x_values = np.arange(range_min,range_max)
+x_values = np.arange(cubic_range_min,cubic_range_max)
 y_values = x_values ** index
 
 
+# Calculate the histogram bin limits.
+# I want to centre a bin on each integer value. I need to find the  
+# centre of the lowest bin and the centre of the highest bin.
+
+# Subtract 0.5 from min & max to round down
+lowest_bin_centre = round(min(distrib_values))
+highest_bin_centre = round(max(distrib_values))
+
+# Generate list of bin edges as floats
+integer_bins = np.arange(lowest_bin_centre - 0.5, highest_bin_centre + 0.5, 1)
+
+
 # Plot the histogram of the normally distributed data points
-plt.hist(distrib_values, label="Normal distribution")
-# Plot the function y=x^2, on the same plot
-plt.plot(x_values,y_values, label="$y=x^3$")
+plt.hist(
+    distrib_values, bins=integer_bins, label="Normal distribution", 
+    color="cyan", edgecolor = "white"
+    )
+
+
+# Plot the function y=x^3, on the same plot
+plt.plot(
+    x_values,y_values, label="$y=x^3$", color="m", 
+    marker="o", linewidth=0.5
+    )
 
 # Add title and labels
 # TeX symbols \mu and \sigma require double escapes inside the string.
@@ -49,7 +69,7 @@ plt.title(
     )
 
 plt.xlabel("X value")
-plt.ylabel("Frequency of value \\ $x^3$")
+plt.ylabel(f"Frequency of value \\ $x^{index}$")
 plt.legend()
 
 # Print to file
